7e5d4cbdb2e652daa3839fc056952467c906b171
slo0ey <rurchi1206@gmail.com>
2025-08-14 20:12:39 +0900

    remove: webVital 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   Outlet,
   RouterProvider,
   createRootRoute,
   createRoute,
   createRouter,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import FormSimpleDemo from './routes/demo.form.simple.tsx'
-import FormAddressDemo from './routes/demo.form.address.tsx'
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import FormSimpleDemo from './routes/demo.form.simple.tsx';
+import FormAddressDemo from './routes/demo.form.address.tsx';
 
-import Header from './components/Header'
+import Header from './components/Header';
 
-import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
+import './styles.css';
 
-import App from './App.tsx'
+import App from './App.tsx';
 
 const rootRoute = createRootRoute({
   component: () => (
@@ -26,19 +25,19 @@ const rootRoute = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
   component: App,
-})
+});
 
 const routeTree = rootRoute.addChildren([
   indexRoute,
   FormSimpleDemo(rootRoute),
   FormAddressDemo(rootRoute),
-])
+]);
 
 const router = createRouter({
   routeTree,
@@ -47,25 +46,24 @@ const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
-})
+});
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-const rootElement = document.getElementById('app')
+const rootElement = document.getElementById('app');
 if (rootElement && !rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
       <RouterProvider router={router} />
     </StrictMode>,
-  )
+  );
 }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
